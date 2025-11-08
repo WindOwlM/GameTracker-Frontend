7412a2a7c2ec4c82f1dda3b9b3aaad59dfbaa7fd
Maicol <mamian642@gmail.com>
2025-11-08 17:13:18 -0500

    Fix: se cambian datos de la caard

--- GameTracker/src/components/cards/card.jsx
+++ GameTracker/src/components/cards/card.jsx
@@ -1,31 +1,39 @@
+import { useState } from 'react'
 import './card.css'
 
-export function GameCard ({id, title,desc, date,genre,platform, image, handleDeleteGame}) {
+export function GameCard ({id, title,desc, date,genres,platform, image, handleDeleteGame}) {
+  const [isEditing,setIsEditing] = useState(false)
+
+  const toggleEditing = () => {
+    setIsEditing(!isEditing)
+  }
+
   return(
     <>
       <div key={id} className="game-card">
         <div className="game-card-image">
-                {title.charAt(0)}
+                {image? <img src={image} alt="" /> : title.charAt(0)}
               </div>
               <div className="game-card-content">
-                <h3 className="game-card-title">{title}</h3>
-                <p className="game-card-description">{desc}</p>
+                <h3 className="game-card-title">{isEditing ? <input type="text" value={title} id="" /> : title}</h3>
+                <p className="game-card-description">{isEditing ? <textarea value={desc} /> : desc}</p>
               <div className="game-card-details">
               <div className="game-detail-item">
-                <span className="game-detail-label">📅 LANZAMIENTO:</span>
-                <span>{date}</span>
+                <span className="game-detail-label">LANZAMIENTO:</span>
+                <span>{isEditing ? <input type='date' value={date} /> : date}</span>
               </div>
               <div className="game-detail-item">
-                <span className="game-detail-label">🎮 GÉNERO:</span>
-                <span>{genre}</span>
+                <span className="game-detail-label">GÉNERO:</span>
+                <span>{genres}</span>
               </div>
               <div className="game-detail-item">
-                <span className="game-detail-label">💻 PLATAFORMA:</span>
+                <span className="game-detail-label">PLATAFORMA:</span>
                 <span>{platform}</span>
               </div>
             </div>
             <div className="game-card-actions">
-            <button className="game-action-btn game-edit-btn">
+            <button className="game-action-btn game-edit-btn"
+            onClick={() => toggleEditing()}>
               <svg className="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
