b7b8afeeb3b54e960431eae7921a536c86b38b04
Maicol <mamian642@gmail.com>
2025-11-12 16:42:18 -0500

    Feature: se arregla la card para mostrar todo y el editar

--- GameTracker/src/components/cards/reviewCard/reviewCard.jsx
+++ GameTracker/src/components/cards/reviewCard/reviewCard.jsx
@@ -1,17 +1,16 @@
 import './reviewCard.css'
+import ModalReview from '../../modal/modalCreateReview/ModalReview'
 
-export function ReviewCard({id,username, title, rating,review,recommend, handleDeleteReview}) {
+export function ReviewCard({id,game_id,username, hoursPlayed,title, rating,review,recommend,difficulty, handleDeleteReview,onUpdate}) {
 
-//   const gameData = {
-//     title,
-//     description: desc,
-//     genres,
-//     platform,
-//     developer,
-//     released,
-//     coverImage: image,
-//     completed
-//   }
+    const reviewData = {
+        game_id,
+        rating,
+        review,
+        recommend,
+        hoursPlayed,
+        difficulty
+    }
 
   return(
     <>
@@ -31,13 +30,22 @@ export function ReviewCard({id,username, title, rating,review,recommend, handleD
         <h1>{title}</h1>
     </div>
     <div className="review-actions">
-        <button className="action-btn edit-btn">
-        <svg className="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-        </svg>
-        Edit
-        </button>
+        <ModalReview 
+        key={id}
+        gameId={game_id}
+        title={title}
+        reviewData={reviewData}
+        onUpdate={onUpdate}
+        onClick={() => onUpdate()}
+        buttonText={
+            <div className="action-btn edit-btn">
+                <svg className="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                </svg>
+                Edit
+            </div>
+        }/>
         <button className="action-btn delete-btn" onClick={() => handleDeleteReview(id)}>
         <svg className="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <polyline points="3 6 5 6 21 6"></polyline>
@@ -49,7 +57,7 @@ export function ReviewCard({id,username, title, rating,review,recommend, handleD
     </div>
     <p className="review-comment">{review}</p>
     <hr />
-    <p className="review-comment">{recommend? "Recomendado" : "NO recomendado"}</p>
+    <p className="review-comment">{recommend? "Recomendado" : "NO recomendado"} |||| Horas jugadas: {hoursPlayed} |||| Dificultad: {difficulty}</p>
     </div>
     </>
   )
